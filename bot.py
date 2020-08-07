44ebef0b7a6c6d9df1c234b35d1bbd370aa43332
Никита Медведев <nikmed@MacBook-Pro.local>
2020-08-08 00:40:37 +0300

    Flask added

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ if "HEROKU" in list(os.environ.keys()):
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url="https://stonks13bot.herokuapp.com/" + TOKEN) # этот url нужно заменить на url вашего Хероку приложения
+        bot.set_webhook(url="https://stonks13bot.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
         return "?", 200
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 else:
