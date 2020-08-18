ed12edc76b6776a76e8bfea7fd7819d8c9ba2d04
Никита Медведев <nikmed@MacBook-Pro.local>
2020-08-18 17:50:39 +0300

    Bot.py server rdy

--- bot.py
+++ bot.py
@@ -140,26 +140,9 @@ def callback_worker(call):
 	db.insert_company(user_id, call.data)
 	
 
+while True:
+	try: 
+		bot.polling(none_stop=True)
+	except Exception as e:
+		print(e)
 
-# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-    @server.route('/' + TOKEN, methods=['POST'])
-    def getMessage():
-    	bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    	return "!", 200
-    @server.route('/')
-    def webhook():
-    	bot.remove_webhook()
-    	bot.set_webhook(url='https://stonks13bot.herokuapp.com/' + TOKEN)
-    	return "!", 200
-
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
